8747e59bbc13456b814417bc647144c52e17d14f
Andreas Holmstedt <holmstedt.andreas@hotmail.com>
2018-04-17 10:13:12 +0200

    first push

---
+++ src/components/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <div class="cards"  >
+      <list-card v-for="(item, index) in dataList" v-on:cardClick="onCardClick" :key="index" :name="item.name" :phone="item.phone" :adress="item.adress" :email="item.email" />
+      <create-card />
+  </div>
+    <div class="selected">
+      <last-clicked />
+  </div>
+  </div>
+</template>
+
+<script>
+  import listCard from './listCard.vue';
+  import lastClicked from './lastClicked.vue';
+  import createCard from './createCard.vue';
+
+  export default {
+    name: 'app',
+    data: function() {
+        return {
+            dataList: [
+              { name: "Robert Karlson",
+                adress: "Beefroad 22, 35352 Queenscounty",
+                phone: "0703374525",
+                email: "robert@karl.son"
+              },
+              { name: "Robert Persson",
+                adress: "Wallawalla 2, 506 36 Svenljunga",
+                phone: "0703346735",
+                email: "robert@persson.se"
+              },
+              { name: "Alfred Svensson",
+                adress: "Porkloin Avenue 25, 13337 Höga Stora",
+                phone: "0706922033",
+                email: "robert@karl.son"
+              },
+              { name: "Calle Munkhammar",
+                adress: "Mountymanroad 15, 40564, Stockholm",
+                phone: "5703374525",
+                email: "calle.munken@gmail.com"
+          }],
+          recentlyClicked: {
+              name: "",
+              adress: "",
+              phone: "",
+              email: ""
+          }
+        }
+    },
+    methods: {
+      onCardClick: function (card) {
+          this.recentlyClicked = {
+            name: card.name,
+            adress: card.adress,
+            phone: card.phone,
+            email: card.email,
+          }
+      }
+    },
+    components: {
+      'list-card': listCard,
+      'last-clicked': lastClicked,
+      'create-card': createCard,
+    },
+  }
+
+</script>
+
+<style src="normalize.css/normalize.css"></style>
+
+<style>
+
+</style>
+
+<style scoped>
+  #app {
+    text-align: center;
+  }
+
+  #app h1 {
+    color: #2c3e50;
+    font-weight: 300;
+    margin: 0;
+  }
+</style>
---
+++ src/components/lastClicked.vue
@@ -0,0 +1,89 @@
+<template>
+  <div v-bind:class="cardStyle">
+    <div v-bind:class="headerStyle">
+      <h2>{{}}</h2>
+      <button>…</button>
+    </div>
+    <div v-bind:class="bottomStyle">
+      <h3></h3>
+      <p></p>
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+     return {
+       cardStyle: "cardStyle1",
+       headerStyle: "headerStyle1",
+       bottomStyle: "bottomStyle1",
+     }
+  },
+
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Lato:300,400,700|Roboto+Slab');
+
+.cardStyle1 {
+  font-family: 'Roboto Slab', serif;
+  font-weight: 300;
+  max-width: 320px;
+  height: 380px;
+  margin: 20px;
+  background-color: #F4F4F4;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.headerStyle1 {
+  margin: 0px;
+  padding: 0px;
+  height: 70px;
+  background-color: #137477;
+}
+
+.headerStyle1 > h2 {
+  font-family: 'Lato', sans-serif;
+  letter-spacing: .3em;
+  font-weight: 700;
+  color: #f3Bdac;
+  margin: 0px;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  text-align: left;
+  padding-left: 1em;
+}
+
+.headerStyle1 > button:first-of-type {
+  position: relative;
+  left: 140px;
+  top: -60px;
+  font-size: 2em;
+  font-weight: 700;
+  transform: rotate(90deg);
+  margin: 0px;
+  padding: 0px;
+  color: #f3Bdac;
+  border: none;
+  background-color: transparent;
+}
+
+.headerStyle1 > button:first-of-type:focus {
+  outline: 0;
+}
+
+.bottomStyle1 {
+    color: #137477;
+    position: relative;
+    border: 3px solid #137477;
+    border-top: none;
+    margin: 0px;
+    padding: 0px;
+    flex:1 1 400px;
+}
+</style>
